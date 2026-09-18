669cc53bfceb7d06eb9617d0a7f00d510ff94511
lastborn <levinachifie016@gmail.com>
2026-09-18 21:28:49 +0300

    booking fix 2

--- lib/screens/bookings_screen.dart
+++ lib/screens/bookings_screen.dart
@@ -52,7 +52,7 @@ class _BookingsScreenState extends State<BookingsScreen> {
     // Watching the store keeps this list in step with cancellations and
     // with requests created from the new-request sheet.
     final store = context.store;
-    final bookings = _filter == BookingStatus.activ
+    final bookings = _filter == BookingStatus.active
         ? store.activeBookings
         : store.bookingsWithStatus(_filter);
 
